d1c4881553abb69bff522e3f4d4557a137472b03
Hafız <ramizovhafiz@gmail.com>
2023-11-26 13:43:51 +0300

    Adding read.me

--- app/src/main/java/com/example/sportsapp/MainActivity.kt
+++ app/src/main/java/com/example/sportsapp/MainActivity.kt
@@ -21,7 +21,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // Burda view model'deki state bilgine eriscem
+            // val uiState by viewModel.uiState.collectAsState()
+              AppTheme {
                   SportsAppPortrait()
+              }
         }
     }
 }
